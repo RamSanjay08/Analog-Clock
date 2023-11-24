60bc489ff0df040c7af8ed8ffeb8ba1bc28e17b3
Ram Sanjay <ramsanjaydev08@gmail.com>
2023-11-24 10:15:15 +0530

    ui done

---
+++ script.js
@@ -0,0 +1,19 @@
+setInterval(setClock,1000)
+
+const getHours = document.querySelector("[data-hours-hand]")
+const getMinutes = document.querySelector("[data-minutes-hand]")
+const getSeconds = document.querySelector("[data-seconds-hand]")
+
+function setClock() {
+  const currentDate = new Date()
+  const secondsRatio = currentDate.getSeconds() / 60
+  const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
+  const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
+  setRotation(getSeconds, secondsRatio);
+  setRotation(getMinutes, minutesRatio);
+  setRotation(getHours, hoursRatio);
+}
+
+function setRotation(element, rotationRatio) {
+  element.style.setProperty("--rotation", rotationRatio * 360)
+}
